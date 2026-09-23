7fd919bf31fcaf1e6d57614d1ff243876bd7f2ab
DHMPROG <aminedahmanepro@gmail.com>
2026-09-23 15:32:44 -0400

    Initial commit - LOG200 Laboratoire TIC-TAC-TOE

---
+++ src/Board.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+// IMPORTANT: Il ne faut pas changer la signature des méthodes
+// de cette classe, ni le nom de la classe.
+// Vous pouvez par contre ajouter d'autres méthodes (ça devrait 
+// être le cas)
+class Board
+{
+    private Mark[][] board;
+
+    // Ne pas changer la signature de cette méthode
+    public Board() {
+        board = new Mark[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = Mark.EMPTY;
+            }
+        }
+
+    }
+
+    // Place la pièce 'mark' sur le plateau, à la
+    // position spécifiée dans Move
+    //
+    // Ne pas changer la signature de cette méthode
+    public void play(Move m, Mark mark){
+        board[m.getRow()][m.getCol()] = mark;
+
+    }
+
+    public ArrayList<Move> getAvailableMoves(){
+        ArrayList<Move> moves = new ArrayList<Move>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == Mark.EMPTY) {
+                    moves.add(new Move(i, j));
+                }
+            }
+        }
+        return moves;
+    }
+
+
+    // retourne  100 pour une victoire
+    //          -100 pour une défaite
+    //           0   pour un match nul
+    // Ne pas changer la signature de cette méthode
+
+    public boolean hasWon(Mark mark){
+        // Check rows and columns
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == mark && board[i][1] == mark && board[i][2] == mark) ||
+                (board[0][i] == mark && board[1][i] == mark && board[2][i] == mark)) {
+                return true;
+            }
+        }
+        // Check diagonals
+        if ((board[0][0] == mark && board[1][1] == mark && board[2][2] == mark) ||
+            (board[0][2] == mark && board[1][1] == mark && board[2][0] == mark)) {
+            return true;
+        }
+        return false;
+    }
+
+    public int evaluate(Mark mark){
+
+        if (true)return 100;
+        else if (false) return -100;
+        else return 0;
+    }
+
+}
